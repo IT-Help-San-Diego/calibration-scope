c7c569b23183271a8b77c5c0001298672d53b014
Carey James Balboa <52982329+careyjames@users.noreply.github.com>
2026-07-12 20:01:55 -0700

    Remove Zig backend and obsolete Zig references

--- zig-backend/bind_wrapper.c
+++
@@ -1,6 +0,0 @@
-#include <sys/socket.h>
-#include <netinet/in.h>
-
-int bind_in(int fd, const struct sockaddr_in *addr, socklen_t len) {
-    return bind(fd, (const struct sockaddr *)addr, len);
-}
--- zig-backend/signal_wrapper.c
+++
@@ -1,5 +0,0 @@
-#include <signal.h>
-
-int set_sigchld_ign(void) {
-    return (int)(long)signal(SIGCHLD, SIG_IGN);
-}
